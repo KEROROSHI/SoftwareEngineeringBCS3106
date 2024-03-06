c67d63874e209059bbef5455bca6e3ad74f9c995
KEROROSHI <bsclmr157023@spu.ac.ke>
2024-03-06 16:41:37 +0300

    Made the admin dashboard

---
+++ Voting_System/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('base.html')
+
+
+@app.route('/admin')
+def admin_login():
+    return render_template('admin_login.html')
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
