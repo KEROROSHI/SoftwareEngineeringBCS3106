9f4e496abebee760917b2f9bcfb937461e930212
KEROROSHI <bsclmr157023@spu.ac.ke>
2024-03-07 11:04:18 +0300

    Reformated the SQL code in the votingsystem database

--- Voting_System/__init__.py
+++ Voting_System/__init__.py
@@ -1,6 +1,12 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask'
+app.config['SECRET_KEY'] = "My secret key"
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -8,13 +14,15 @@ def hello_world():  # put application's code here
     return render_template('base.html')
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin_login():
     return render_template('admin_login.html')
+
 
 @app.route('/admin_dashboard')
 def admin_dashboard():
     return render_template('admin_dashboard.html')
+
 
 @app.route('/voter')
 def voter_login():
