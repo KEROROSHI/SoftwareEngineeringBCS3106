cbc2bf06aeb2e1d55038ed7c932908266e052084
keziamumbi <160420890+keziamumbi@users.noreply.github.com>
2024-03-18 15:09:24 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+# Importing the 'app' object from the module 'Voting_System'.
 from Voting_System import app
 
 if __name__ == "__main__":
+    # Running the Flask application represented by the 'app' object in debug mode,
+    # which provides helpful debugging information on errors.
+    # The application is set to run on port 5001.
     app.run(debug=True,port=5001)
