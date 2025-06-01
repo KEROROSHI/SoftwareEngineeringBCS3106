f41a84066c20c321ce4ae960edbeaaf0c91b808c
KEROROSHI <bsclmr157023@spu.ac.ke>
2025-06-02 01:20:19 +0300

    Modified __init__.py

--- Voting_System/__init__.py
+++ Voting_System/__init__.py
@@ -102,7 +102,7 @@ def seed_admin_from_config(config_path="config/admin_config.json"):
 
             insert_query = """
             INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`, `voting_session_id`)
-            VALUES (%d, %s, %s, %s, %s, %s, %s, %d)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """
             cursor.execute(insert_query, (
                 admin_data["id"],
@@ -216,7 +216,7 @@ mysql_conn = connect_to_db()
 def setup_database():
     create_database_if_not_exists()
     create_admin_table_if_not_exists(mysql_conn)
-    seed_admin_from_config("config/admin_config.json")  # Ensure the config directory exists in your image
+    seed_admin_from_config("admin_config.json")  # Ensure the config directory exists in your image
     create_candidates_table_if_not_exists(mysql_conn)
     create_positions_table_if_not_exists(mysql_conn)
     create_session_table_if_not_exists(mysql_conn)
